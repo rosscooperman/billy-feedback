460a280eeaf3080d691ec0056217bc99fd655b45
Ross Cooperman <cooperman@gmail.com>
2012-09-07 15:10:52 -0400

    still store files locally in development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,16 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJNU5CLXDPYM3ZZVQ',
-    :aws_secret_access_key  => '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'
-  }
-  config.fog_directory  = 'billyup-feedback'
+  if RACK_ENV.to_sym == :production
+    config.storage         = :fog
+    config.fog_directory   = 'billyup-feedback'
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => 'AKIAJNU5CLXDPYM3ZZVQ',
+      :aws_secret_access_key  => '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'
+    }
+  else
+    config.root = "#{APP_ROOT}/public"
+    config.storage = :file
+  end
 end
--- lib/image_uploader.rb
+++ lib/image_uploader.rb
@@ -1,6 +1,4 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  storage :fog     # :fog for production
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
